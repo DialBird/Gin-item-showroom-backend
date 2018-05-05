d83e5b6bc196628fd6fd2eb90e233e0563b81e79
keisuke taniguchi <rememberthatk@gmail.com>
2018-05-05 10:38:19 +0000

    Add Image UPload Demo Path

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gopkg.in/mgo.v2"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -13,6 +19,44 @@ func loadDotenv() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func sdkHandler(c *gin.Context) {
+	svc := s3.New(
+		session.Must(session.NewSession()),
+		aws.NewConfig().WithRegion("ap-northeast-1"),
+	)
+	file, err := os.Open("test.jpg")
+	if err != nil {
+		fmt.Printf("err opening file: %s", err)
+	}
+	defer file.Close()
+
+	fileInfo, _ := file.Stat()
+	var size = fileInfo.Size()
+	buffer := make([]byte, size)
+	file.Read(buffer)
+	fileBytes := bytes.NewReader(buffer)
+	fileType := http.DetectContentType(buffer)
+	path := "/media/" + file.Name()
+
+	input := &s3.PutObjectInput{
+		Body:                 fileBytes,
+		Bucket:               aws.String(os.Getenv("BUCKET_NAME")),
+		Key:                  aws.String(path),
+		ContentLength:        aws.Int64(size),
+		ContentType:          aws.String(fileType),
+		ServerSideEncryption: aws.String("AES256"),
+		Tagging:              aws.String("key1=value1&key2=value2"),
+	}
+	result, err := svc.PutObject(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(result)
+
+	c.String(200, "Image Sended")
 }
 
 func main() {
@@ -26,6 +70,8 @@ func main() {
 	se.SetMode(mgo.Monotonic, true)
 
 	r := gin.Default()
+
+	r.GET("sdk", sdkHandler)
 
 	SetItemRouter(r, se)
 
